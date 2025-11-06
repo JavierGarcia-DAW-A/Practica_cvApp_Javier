c8a9b6980dbc94186a9d2632c4879d7dcdc99b18
JavierGarcia-DAW-A <jgarsani@ieszaidinvergeles.org>
2025-11-06 12:40:56 +0100

    Editar modelo

--- cvApp_Javier/cvApp_Javier/app/Models/Curriculum.php
+++ cvApp_Javier/cvApp_Javier/app/Models/Curriculum.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curriculum extends Model
 {
-    //
+    protected $table = 'alumnos';
+
+    protected $fillable = ['nombre', 'apellidos', 'telefono', 'correo', 'fecha_nacimiento', 'nota_media', 'experiencia', 'formacion', 'habilidades', 'pdf', 'fotografia'];
+
+    public function getFotoUrl()
+    {
+        if ($this->fotografia) {
+            return asset('storage/' . $this->fotografia);
+        }
+        return asset('assets/img/sin_foto.png');
+    }
+
+    public function getPdfUrl()
+    {
+        if ($this->pdf) {
+            return asset('storage/' . $this->pdf);
+        }
+        return null;
+    }
 }
